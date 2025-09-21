6d3cd88eb2e0856d9fded0a4bc8e729695b19eba
Bastien59960 <42418271+Bastien59960@users.noreply.github.com>
2025-09-21 15:32:54 -0500

    Update ext.php

--- ext.php
+++ ext.php
@@ -13,30 +13,30 @@ namespace bastien59960\reactions;
 */
 class ext extends \phpbb\extension\base
 {
-    /**
-    * Check whether the extension can be enabled.
-    * The current phpBB version should meet or exceed
-    * the minimum version required by this extension.
-    *
-    * Requires phpBB 3.3.0 due to updated extension meta-data.
-    *
-    * @return bool
-    */
-    public function is_enableable()
-    {
-        $config = $this->container->get('config');
-        return phpbb_version_compare($config['version'], '3.3.0', '>=');
-    }
+	/**
+	* Check whether the extension can be enabled.
+	* The current phpBB version should meet or exceed
+	* the minimum version required by this extension.
+	*
+	* Requires phpBB 3.3.0 due to updated extension meta-data.
+	*
+	* @return bool
+	*/
+	public function is_enableable()
+	{
+		$config = $this->container->get('config');
+		return phpbb_version_compare($config['version'], '3.3.0', '>=');
+	}
 
-    /**
-    * Return the current version of the extension.
-    *
-    * This should match the version used in the migration class.
-    *
-    * @return string
-    */
-    public function get_version()
-    {
-        return '1.0.0';
-    }
+	/**
+	* Return the current version of the extension.
+	*
+	* This should match the version used in the migration class.
+	*
+	* @return string
+	*/
+	public function get_version()
+	{
+		return '1.0.0';
+	}
 }
