ceef9bd7687ae38a420758eda83ef78a68d4f2ec
Bastien59960 <42418271+Bastien59960@users.noreply.github.com>
2025-09-21 16:28:25 -0500

    Update reactions.js

--- assets/js/reactions.js
+++ assets/js/reactions.js
@@ -9,21 +9,23 @@
             e.preventDefault();
 
             var $button = $(this);
-            var postId = $button.closest('.reactions').data('post-id');
+            var $reactionsContainer = $button.closest('.reactions');
+            var postId = $reactionsContainer.data('post-id');
             var reactionUnicode = $button.data('reaction');
 
             // Appel de la fonction pour envoyer la requête AJAX
-            sendReaction(postId, reactionUnicode);
+            sendReaction(postId, reactionUnicode, $reactionsContainer);
         });
     }
 
     /**
-     * Envoie la requête AJAX au contrôleur PHP.
+     * Envoie la requête AJAX au contrôleur PHP et met à jour l'interface.
      * @param {number} postId ID du message.
      * @param {string} reactionUnicode Code Unicode de la réaction.
+     * @param {jQuery} $container Le conteneur jQuery des réactions pour ce post.
      */
-    function sendReaction(postId, reactionUnicode) {
-        var url = '{{ U_REACTIONS_HANDLE }}'; // Variable de template pour l'URL
+    function sendReaction(postId, reactionUnicode, $container) {
+        var url = '{{ U_REACTIONS_HANDLE }}';
 
         $.ajax({
             url: url,
@@ -33,14 +35,36 @@
                 reaction: reactionUnicode,
             },
             success: function(response) {
-                console.log('Réponse du serveur:', response);
-
-                // Ici, on mettra à jour l'interface utilisateur
+                if (response.status === 'success') {
+                    // Si la requête a réussi, on met à jour l'affichage
+                    updateReactionsDisplay(response.counters, $container);
+                } else {
+                    // Gérer les erreurs, par exemple afficher un message d'erreur
+                    console.error('Erreur du serveur:', response.message);
+                }
             },
             error: function(xhr, status, error) {
                 console.error('Erreur AJAX:', error);
             }
         });
+    }
+
+    /**
+     * Met à jour l'affichage des compteurs et des boutons de réaction.
+     * @param {object} counters Les compteurs de réactions reçus du serveur.
+     * @param {jQuery} $container Le conteneur jQuery des réactions pour ce post.
+     */
+    function updateReactionsDisplay(counters, $container) {
+        $container.empty(); // On vide le conteneur actuel
+        
+        // On recrée les boutons de réaction avec les nouveaux compteurs
+        for (var unicode in counters) {
+            if (counters.hasOwnProperty(unicode)) {
+                var count = counters[unicode];
+                var html = '<span class="reaction-button" data-reaction="' + unicode + '">' + unicode + ' <span class="reaction-count">' + count + '</span></span>';
+                $container.append(html);
+            }
+        }
     }
 
     $(document).ready(function() {
