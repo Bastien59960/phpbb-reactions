351a9209db49eb0345fa93dfecce5b94af71e227
Bastien59960 <42418271+Bastien59960@users.noreply.github.com>
2025-09-22 13:43:18 -0500

    Update listener.php

--- event/listener.php
+++ event/listener.php
@@ -12,32 +12,54 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class listener implements EventSubscriberInterface
 {
+    /** @var \phpbb\db\driver\driver_interface */
+    protected $db;
+    
+    /** @var \phpbb\user */
+    protected $user;
+    
+    /** @var string */
+    protected $post_reactions_table;
+    
+    /** @var string */
+    protected $posts_table;
+    
     /** @var \phpbb\template\template */
     protected $template;
     
-    /** @var \phpbb\path_helper */
-    protected $path_helper;
+    /** @var \phpbb\language\language */
+    protected $language;
     
-    /** @var string */
-    protected $ext_path;
-    
-    /** @var \phpbb\request\request */
-    protected $request;
+    /** @var \phpbb\controller\helper */
+    protected $helper;
 
     /**
      * Constructor
      *
+     * @param \phpbb\db\driver\driver_interface $db
+     * @param \phpbb\user $user
+     * @param string $post_reactions_table
+     * @param string $posts_table
      * @param \phpbb\template\template $template
-     * @param \phpbb\path_helper $path_helper  
-     * @param \phpbb\request\request $request
-     * @param string $ext_path
+     * @param \phpbb\language\language $language
+     * @param \phpbb\controller\helper $helper
      */
-    public function __construct(\phpbb\template\template $template, \phpbb\path_helper $path_helper, \phpbb\request\request $request, $ext_path)
-    {
+    public function __construct(
+        \phpbb\db\driver\driver_interface $db,
+        \phpbb\user $user,
+        $post_reactions_table,
+        $posts_table,
+        \phpbb\template\template $template,
+        \phpbb\language\language $language,
+        \phpbb\controller\helper $helper
+    ) {
+        $this->db = $db;
+        $this->user = $user;
+        $this->post_reactions_table = $post_reactions_table;
+        $this->posts_table = $posts_table;
         $this->template = $template;
-        $this->path_helper = $path_helper;
-        $this->request = $request;
-        $this->ext_path = $ext_path;
+        $this->language = $language;
+        $this->helper = $helper;
     }
 
     /**
@@ -49,49 +71,54 @@ class listener implements EventSubscriberInterface
     {
         return [
             'core.page_header' => 'add_assets_to_page',
-            'core.viewtopic_cache_user_data' => 'load_language',
+            'core.viewtopic_cache_user_data' => 'load_language_and_data',
             'core.viewtopic_post_row_after' => 'display_reactions',
+            'core.viewforum_modify_topicrow' => 'add_forum_data',
         ];
     }
 
     /**
-     * Load language file
-     *
-     * @param \phpbb\event\data $event
-     */
-    public function load_language($event)
-    {
-        // Charger le fichier de langue de votre extension
-        global $user;
-        $user->add_lang_ext('bastien59960/reactions', 'common');
-    }
-
-    /**
-     * Add assets to page
+     * Load language and add assets to page
      *
      * @param \phpbb\event\data $event
      */
     public function add_assets_to_page($event)
     {
-        // Vérifier si nous sommes sur une page qui a besoin des assets
-        $page_name = $this->request->variable('page_name', '');
+        // Charger le fichier de langue
+        $this->language->add_lang('common', 'bastien59960/reactions');
         
-        if (strpos($this->request->server('REQUEST_URI'), 'viewtopic') !== false || 
-            strpos($this->request->server('REQUEST_URI'), 'viewforum') !== false)
-        {
-            // Construire les chemins vers les assets
-            $css_path = $this->path_helper->update_web_root_path($this->ext_path . 'styles/prosilver/theme/reactions.css');
-            $js_path = $this->path_helper->update_web_root_path($this->ext_path . 'styles/prosilver/template/js/reactions.js');
-            
-            // Assigner les variables au template
-            $this->template->assign_vars([
-                'S_REACTIONS_ENABLED' => true,
-                'REACTIONS_CSS_PATH' => $css_path,
-                'REACTIONS_JS_PATH' => $js_path,
-            ]);
-        }
+        // Construire les chemins des assets - méthode compatible phpBB 3.3
+        $web_root_path = $this->helper->get_current_url();
+        $ext_web_path = str_replace(generate_board_url() . '/', '', $this->helper->route('bastien59960_reactions_controller', [], false));
+        $ext_web_path = dirname($ext_web_path) . '/';
+        
+        // Alternative plus simple : utiliser les chemins relatifs
+        $css_path = './ext/bastien59960/reactions/styles/prosilver/theme/reactions.css';
+        $js_path = './ext/bastien59960/reactions/styles/prosilver/template/js/reactions.js';
+        
+        // Assigner les variables au template - MÉTHODE CORRECTE pour phpBB 3.3
+        $this->template->assign_vars([
+            'S_REACTIONS_ENABLED' => true,
+            'REACTIONS_CSS_PATH' => $css_path,
+            'REACTIONS_JS_PATH' => $js_path,
+            'U_REACTIONS_AJAX' => $this->helper->route('bastien59960_reactions_ajax', []),
+        ]);
     }
     
+    /**
+     * Load language and user data
+     *
+     * @param \phpbb\event\data $event
+     */
+    public function load_language_and_data($event)
+    {
+        // Charger des données supplémentaires si nécessaire
+        $user_cache_data = $event['user_cache_data'];
+        
+        // Vous pouvez ajouter des données utilisateur ici si besoin
+        $event['user_cache_data'] = $user_cache_data;
+    }
+
     /**
      * Display reactions on posts
      *
@@ -101,15 +128,90 @@ class listener implements EventSubscriberInterface
     {
         $post_row = $event['post_row'];
         $row = $event['row'];
+        $post_id = $row['post_id'];
         
-        // Ici vous ajouteriez la logique pour récupérer et afficher les réactions
-        // Exemple basique :
+        // Récupérer les réactions pour ce post
+        $reactions = $this->get_post_reactions($post_id);
+        $user_reactions = $this->get_user_reactions($post_id, $this->user->data['user_id']);
         
+        // Préparer les données pour le template
+        $reactions_data = [];
+        foreach ($reactions as $emoji => $count) {
+            $reactions_data[] = [
+                'EMOJI' => $emoji,
+                'COUNT' => $count,
+                'USER_REACTED' => in_array($emoji, $user_reactions),
+            ];
+        }
+        
+        // Ajouter aux données du post
         $post_row = array_merge($post_row, [
             'S_REACTIONS_ENABLED' => true,
-            'POST_REACTIONS' => '', // Vos données de réactions ici
+            'POST_REACTIONS' => $reactions_data,
+            'U_REACT' => $this->helper->route('bastien59960_reactions_add', ['post_id' => $post_id]),
         ]);
         
         $event['post_row'] = $post_row;
+    }
+    
+    /**
+     * Add forum-level data if needed
+     *
+     * @param \phpbb\event\data $event
+     */
+    public function add_forum_data($event)
+    {
+        // Ajouter des données au niveau du forum si nécessaire
+    }
+    
+    /**
+     * Get reactions count for a post
+     *
+     * @param int $post_id
+     * @return array
+     */
+    private function get_post_reactions($post_id)
+    {
+        $sql = 'SELECT reaction_emoji, COUNT(*) as reaction_count
+                FROM ' . $this->post_reactions_table . '
+                WHERE post_id = ' . (int) $post_id . '
+                GROUP BY reaction_emoji';
+        $result = $this->db->sql_query($sql);
+        
+        $reactions = [];
+        while ($row = $this->db->sql_fetchrow($result)) {
+            $reactions[$row['reaction_emoji']] = (int) $row['reaction_count'];
+        }
+        $this->db->sql_freeresult($result);
+        
+        return $reactions;
+    }
+    
+    /**
+     * Get user reactions for a post
+     *
+     * @param int $post_id
+     * @param int $user_id
+     * @return array
+     */
+    private function get_user_reactions($post_id, $user_id)
+    {
+        if ($user_id == ANONYMOUS) {
+            return [];
+        }
+        
+        $sql = 'SELECT reaction_emoji
+                FROM ' . $this->post_reactions_table . '
+                WHERE post_id = ' . (int) $post_id . '
+                    AND user_id = ' . (int) $user_id;
+        $result = $this->db->sql_query($sql);
+        
+        $reactions = [];
+        while ($row = $this->db->sql_fetchrow($result)) {
+            $reactions[] = $row['reaction_emoji'];
+        }
+        $this->db->sql_freeresult($result);
+        
+        return $reactions;
     }
 }
