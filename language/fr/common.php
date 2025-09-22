d6bcd503081845f0687888944497b3251530f572
Bastien59960 <42418271+Bastien59960@users.noreply.github.com>
2025-09-22 13:55:10 -0500

    Update common.php

--- language/fr/common.php
+++ language/fr/common.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Reactions Extension for phpBB 3.3
+ * Fichier de langue français
+ * 
+ * @copyright (c) 2025 Bastien59960
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ */
+
 if (!defined('IN_PHPBB'))
 {
     exit;
@@ -9,9 +17,26 @@ if (empty($lang) || !is_array($lang))
     $lang = array();
 }
 
+// Fusionner le tableau de langues existant
 $lang = array_merge($lang, array(
-    'REACTION_ADD'        => 'Ajouter une réaction',
-    'REACTION_REMOVE'     => 'Retirer votre réaction', 
-    'REACTION_MORE'       => '+',
-    'POST_REACTIONS'      => 'Réactions aux posts',
+    'REACTION_ADD'              => 'Ajouter une réaction',
+    'REACTION_REMOVE'           => 'Retirer votre réaction',
+    'REACTION_MORE'             => 'Plus de réactions',
+    'REACTION_LOADING'          => 'Chargement...',
+    'REACTION_ERROR'            => 'Erreur lors de la réaction',
+    'REACTION_SUCCESS_ADD'      => 'Réaction ajoutée avec succès',
+    'REACTION_SUCCESS_REMOVE'   => 'Réaction supprimée avec succès',
+    'REACTION_NOT_AUTHORIZED'   => 'Vous n\'êtes pas autorisé à réagir',
+    'REACTION_INVALID_POST'     => 'Post invalide',
+    'REACTION_INVALID_EMOJI'    => 'Emoji invalide',
+    'REACTION_ALREADY_EXISTS'   => 'Vous avez déjà réagi avec cet emoji',
+    'REACTION_NOT_FOUND'        => 'Réaction non trouvée',
+    'REACTION_COUNT_SINGULAR'   => '%d réaction',
+    'REACTION_COUNT_PLURAL'     => '%d réactions',
+    'REACTIONS_TITLE'           => 'Réactions',
+    'NO_REACTIONS'              => 'Aucune réaction pour le moment',
+    'REACTIONS_BY_USERS'        => 'Réactions des utilisateurs',
+    'REACTION_BY_USER'          => 'Réaction de %s',
+    'REACTIONS_SEPARATOR'       => ', ',
+    'REACTION_AND'              => ' et ',
 ));
