9a465a5887987e07783500422b8eeb99b0ce5584
Bastien59960 <42418271+Bastien59960@users.noreply.github.com>
2025-09-22 13:41:33 -0500

    Update listener.php

--- event/listener.php
+++ event/listener.php
@@ -21,17 +21,22 @@ class listener implements EventSubscriberInterface
     /** @var string */
     protected $ext_path;
     
+    /** @var \phpbb\request\request */
+    protected $request;
+
     /**
      * Constructor
      *
      * @param \phpbb\template\template $template
-     * @param \phpbb\path_helper $path_helper
+     * @param \phpbb\path_helper $path_helper  
+     * @param \phpbb\request\request $request
      * @param string $ext_path
      */
-    public function __construct(\phpbb\template\template $template, \phpbb\path_helper $path_helper, $ext_path)
+    public function __construct(\phpbb\template\template $template, \phpbb\path_helper $path_helper, \phpbb\request\request $request, $ext_path)
     {
         $this->template = $template;
         $this->path_helper = $path_helper;
+        $this->request = $request;
         $this->ext_path = $ext_path;
     }
 
@@ -44,8 +49,21 @@ class listener implements EventSubscriberInterface
     {
         return [
             'core.page_header' => 'add_assets_to_page',
-            // Ajoutez d'autres événements selon vos besoins
+            'core.viewtopic_cache_user_data' => 'load_language',
+            'core.viewtopic_post_row_after' => 'display_reactions',
         ];
+    }
+
+    /**
+     * Load language file
+     *
+     * @param \phpbb\event\data $event
+     */
+    public function load_language($event)
+    {
+        // Charger le fichier de langue de votre extension
+        global $user;
+        $user->add_lang_ext('bastien59960/reactions', 'common');
     }
 
     /**
@@ -55,21 +73,43 @@ class listener implements EventSubscriberInterface
      */
     public function add_assets_to_page($event)
     {
-        // Méthode corrigée pour ajouter les assets CSS et JS
+        // Vérifier si nous sommes sur une page qui a besoin des assets
+        $page_name = $this->request->variable('page_name', '');
         
-        // Ajouter le CSS
-        $this->template->assign_vars([
-            'S_REACTIONS_CSS' => $this->path_helper->update_web_root_path($this->ext_path . 'styles/prosilver/theme/reactions.css'),
+        if (strpos($this->request->server('REQUEST_URI'), 'viewtopic') !== false || 
+            strpos($this->request->server('REQUEST_URI'), 'viewforum') !== false)
+        {
+            // Construire les chemins vers les assets
+            $css_path = $this->path_helper->update_web_root_path($this->ext_path . 'styles/prosilver/theme/reactions.css');
+            $js_path = $this->path_helper->update_web_root_path($this->ext_path . 'styles/prosilver/template/js/reactions.js');
+            
+            // Assigner les variables au template
+            $this->template->assign_vars([
+                'S_REACTIONS_ENABLED' => true,
+                'REACTIONS_CSS_PATH' => $css_path,
+                'REACTIONS_JS_PATH' => $js_path,
+            ]);
+        }
+    }
+    
+    /**
+     * Display reactions on posts
+     *
+     * @param \phpbb\event\data $event
+     */
+    public function display_reactions($event)
+    {
+        $post_row = $event['post_row'];
+        $row = $event['row'];
+        
+        // Ici vous ajouteriez la logique pour récupérer et afficher les réactions
+        // Exemple basique :
+        
+        $post_row = array_merge($post_row, [
+            'S_REACTIONS_ENABLED' => true,
+            'POST_REACTIONS' => '', // Vos données de réactions ici
         ]);
         
-        // Ajouter le JavaScript
-        $this->template->assign_vars([
-            'S_REACTIONS_JS' => $this->path_helper->update_web_root_path($this->ext_path . 'styles/prosilver/template/js/reactions.js'),
-        ]);
-        
-        // Alternative pour ajouter directement dans le template
-        // Vous devrez inclure ceci dans votre template overall_header_head_append.html :
-        // <link href="{S_REACTIONS_CSS}" rel="stylesheet" type="text/css" media="screen" />
-        // <script src="{S_REACTIONS_JS}"></script>
+        $event['post_row'] = $post_row;
     }
 }
