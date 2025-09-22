d7b6034cd7a13fa5af014095cbd0e5bf6dbcdddc
Bastien59960 <42418271+Bastien59960@users.noreply.github.com>
2025-09-21 19:14:11 -0500

    Update listener.php

--- event/listener.php
+++ event/listener.php
@@ -1,10 +1,21 @@
-<?php
 namespace bastien59960\reactions\event;
 
-class listener
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class listener implements EventSubscriberInterface
 {
+    static public function getSubscribedEvents()
+    {
+        return [
+            'core.viewtopic_modify_post_row' => 'test_event',
+        ];
+    }
+
     public function test_event($event)
     {
-        $event['post_row']['REACTIONS'] = '<div style="background:red;color:white;padding:10px;margin:5px 0;">TEST ÉVÉNEMENT PHP FONCTIONNE!</div>';
+        // Exemple : ajouter un template statique après le message
+        $post_row = $event['post_row'];
+        $post_row['MESSAGE'] .= '<div style="background:blue;color:white;">TEMPLATE STATIQUE CHARGÉ (BON EMPLACEMENT) !</div>';
+        $event['post_row'] = $post_row;
     }
 }
