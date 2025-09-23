d97e3d0d1da61241f99fcebb73aefb1c274565bd
Bastien59960 <42418271+Bastien59960@users.noreply.github.com>
2025-09-23 14:55:27 -0500

    Update test.php

--- controller/test.php
+++ controller/test.php
@@ -1,3 +1,43 @@
+<?php
+/**
+ * Reactions Extension for phpBB 3.3
+ * AJAX Controller
+ * * @copyright (c) 2025 Bastien59960
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ */
+
+namespace bastien59960\reactions\controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ajax
+{
+    /** @var \phpbb\db\driver\driver_interface */
+    protected $db;
+    
+    /** @var \phpbb\user */
+    protected $user;
+    
+    /** @var \phpbb\request\request */
+    protected $request;
+    
+    /** @var \phpbb\auth\auth */
+    protected $auth;
+    
+    /** @var \phpbb\language\language */
+    protected $language;
+    
+    /** @var string */
+    protected $post_reactions_table;
+    
+    /** @var string */
+    protected $posts_table;
+    protected $topics_table; 
+    protected $forums_table;
+    
+    protected $root_path; 
+    protected $php_ext; 
 public function handle()
 {
     return new JsonResponse(['status' => 'OK']);
