0d68c7e7ac11ac61bc72386f6ae59cecd2d395bf
Bastien59960 <42418271+Bastien59960@users.noreply.github.com>
2025-10-06 18:15:47 -0500

    Create ucp_reactions.php

---
+++ ext/bastien59960/reactions/language/fr/ucp_reactions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Fichier de langue FR - Configuration UCP des réactions
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+$lang = array_merge($lang, array(
+	'UCP_REACTIONS_TITLE'		=> 'Notifications de réactions',
+	'UCP_REACTIONS_EXPLAIN'		=> 'Ici, vous pouvez choisir de recevoir ou non les notifications de réactions.',
+	'UCP_REACTIONS_NOTIFY'		=> 'Recevoir les notifications dans le forum (cloche)',
+	'UCP_REACTIONS_EMAIL'		=> 'Recevoir un e-mail pour chaque réaction',
+	'UCP_REACTIONS_SAVED'		=> 'Vos préférences de notifications ont été enregistrées.',
+));
