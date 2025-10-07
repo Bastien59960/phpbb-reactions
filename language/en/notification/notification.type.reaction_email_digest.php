966e24a973d40f1a48bdc26d77d8e4582773eb97
Bastien59960 <42418271+Bastien59960@users.noreply.github.com>
2025-10-07 02:19:25 -0500

    Create notification.type.reaction_email_digest.php

---
+++ language/en/notification/notification.type.reaction_email_digest.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('IN_PHPBB')) exit;
+
+$lang = array_merge($lang, array(
+    'NOTIFICATION_TYPE_NOTIFICATION.TYPE.REACTION_EMAIL_DIGEST' => 'Reaction email digest',
+    'NOTIFICATION_REACTION_EMAIL_DIGEST_TITLE' => 'Reaction email digest',
+    'NOTIFICATION_REACTION_EMAIL_DIGEST_DESC'  => 'Receive a periodic email summary of the reactions received on your posts.',
+));
