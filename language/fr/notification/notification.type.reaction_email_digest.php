5fc8539a5caf056c1a216b4f0a6c2214f95d9c63
Bastien59960 <42418271+Bastien59960@users.noreply.github.com>
2025-10-07 02:18:59 -0500

    Create notification.type.reaction_email_digest.php

---
+++ language/fr/notification/notification.type.reaction_email_digest.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('IN_PHPBB')) exit;
+
+$lang = array_merge($lang, array(
+    'NOTIFICATION_TYPE_NOTIFICATION.TYPE.REACTION_EMAIL_DIGEST' => 'Résumé e-mail des réactions',
+    'NOTIFICATION_REACTION_EMAIL_DIGEST_TITLE' => 'Résumé e-mail des réactions',
+    'NOTIFICATION_REACTION_EMAIL_DIGEST_DESC'  => 'Recevoir un résumé périodique par e-mail des réactions reçues sur vos messages.',
+));
