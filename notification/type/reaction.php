9e5d33c4c3d5faee33493ad515c575ca2392fcc8
Bastien59960 <bastien@debucquoi.com>
2025-10-16 16:22:00 -0500

    Refactor ajax class constructor to restore reactions_helper parameter and improve code clarity. Update reaction class constructor to maintain correct parameter order for better readability.

--- notification/type/reaction.php
+++ notification/type/reaction.php
@@ -113,7 +113,7 @@ class reaction extends base
         // L'ordre correct est : user, language, db, auth, root_path, php_ext, notifications_table
 		parent::__construct(
 			$user,
-			$language, // <- Doit être en 2ème position
+			$language,
 			$db,
 			$auth,
 			$phpbb_root_path,
